1e0f445c9e6e3a86b97bbcae61b59fc21dd01b23
tankcr <kristopher.roy@gmail.com>
2019-07-24 12:51:11 -0700

    added about comments

--- Create_Scheduled_Task.ps1
+++ Create_Scheduled_Task.ps1
@@ -1,3 +1,6 @@
-﻿#
-# Script.ps1
-#
+﻿<#
+	Create_Scheduled_Task.ps1
+	Created by - Kristopher C. Roy
+	Created on - 24 Jul 2019
+	Purpose - To provide a GUI driven function to help create scheduled tasks
+#>
