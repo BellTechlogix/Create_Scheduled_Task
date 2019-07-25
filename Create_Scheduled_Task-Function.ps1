2fa19b9e35ccaf8ce62d91c810fd1491cdc50edd
tankcr <kristopher.roy@gmail.com>
2019-07-25 11:58:01 -0700

    funished runable version and added zip with .bat

--- Create_Scheduled_Task-Function.ps1
+++ Create_Scheduled_Task-Function.ps1
@@ -1,8 +1,8 @@
-<#
+﻿<#
 	Create_Scheduled_Task-Function.ps1
 	Created by - Kristopher C. Roy
-	Created on - 25 Jul 2019
-	Purpose - To provide a GUI driven script to help create scheduled tasks
+	Created on - 24 Jul 2019
+	Purpose - To provide a GUI driven function to help create scheduled tasks
 #>
 
 #Function to create a scheduled task in task scheduler
@@ -230,96 +230,3 @@ Function New-Scheduledtask($Name,[Parameter()][ValidateSet('Daily','Weekly','Mon
     #    $task | Set-ScheduledTask	
 	#}
 }
-
-#Multiple Selection Box
-Function MultipleSelectionBox ($inputarray,$prompt,$listboxtype) {
- 
-# Taken from Technet - http://technet.microsoft.com/en-us/library/ff730950.aspx
-# This version has been updated to work with Powershell v3.0.
-# Had to replace $x with $Script:x throughout the function to make it work. 
-# This specifies the scope of the X variable.  Not sure why this is needed for v3.
-# http://social.technet.microsoft.com/Forums/en-SG/winserverpowershell/thread/bc95fb6c-c583-47c3-94c1-f0d3abe1fafc
-#
-# Function has 3 inputs:
-#     $inputarray = Array of values to be shown in the list box.
-#     $prompt = The title of the list box
-#     $listboxtype = system.windows.forms.selectionmode (None, One, MutiSimple, or MultiExtended)
- 
-$Script:x = @()
- 
-[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
-[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
- 
-$objForm = New-Object System.Windows.Forms.Form 
-$objForm.Text = $prompt
-$objForm.Size = New-Object System.Drawing.Size(300,600) 
-$objForm.StartPosition = "CenterScreen"
- 
-$objForm.KeyPreview = $True
- 
-$objForm.Add_KeyDown({if ($_.KeyCode -eq "Enter") 
-    {
-        foreach ($objItem in $objListbox.SelectedItems)
-            {$Script:x += $objItem}
-        $objForm.Close()
-    }
-    })
- 
-$objForm.Add_KeyDown({if ($_.KeyCode -eq "Escape") 
-    {$objForm.Close()}})
- 
-$OKButton = New-Object System.Windows.Forms.Button
-$OKButton.Location = New-Object System.Drawing.Size(75,520)
-$OKButton.Size = New-Object System.Drawing.Size(75,23)
-$OKButton.Text = "OK"
- 
-$OKButton.Add_Click(
-   {
-        foreach ($objItem in $objListbox.SelectedItems)
-            {$Script:x += $objItem}
-        $objForm.Close()
-   })
- 
-$objForm.Controls.Add($OKButton)
- 
-$CancelButton = New-Object System.Windows.Forms.Button
-$CancelButton.Location = New-Object System.Drawing.Size(150,520)
-$CancelButton.Size = New-Object System.Drawing.Size(75,23)
-$CancelButton.Text = "Cancel"
-$CancelButton.Add_Click({$objForm.Close()})
-$objForm.Controls.Add($CancelButton)
- 
-$objLabel = New-Object System.Windows.Forms.Label
-$objLabel.Location = New-Object System.Drawing.Size(10,20) 
-$objLabel.Size = New-Object System.Drawing.Size(280,20) 
-$objLabel.Text = "Please make a selection from the list below:"
-$objForm.Controls.Add($objLabel) 
- 
-$objListbox = New-Object System.Windows.Forms.Listbox 
-$objListbox.Location = New-Object System.Drawing.Size(10,40) 
-$objListbox.Size = New-Object System.Drawing.Size(260,20) 
- 
-$objListbox.SelectionMode = $listboxtype
- 
-$inputarray | ForEach-Object {[void] $objListbox.Items.Add($_)}
- 
-$objListbox.Height = 470
-$objForm.Controls.Add($objListbox) 
-$objForm.Topmost = $True
- 
-$objForm.Add_Shown({$objForm.Activate()})
-[void] $objForm.ShowDialog()
- 
-Return $Script:x
-}
-
-#Typed Inputbox Function
-Function InputBox($title,$body,$default){
-	Add-Type -AssemblyName Microsoft.VisualBasic
-	Add-Type -AssemblyName PresentationCore,PresentationFramework
-	[Microsoft.VisualBasic.Interaction]::InputBox($body, $title, $default)
-}
-
-$user = $ENV:USERNAME
-New-ScheduledTask -Name (InputBox -title "Scheduled Task Name" -body "Please Type the Name of your Scheduled Task" -default "$User Created Task") -Frequency (MultipleSelectionBox -inputarray "Daily","Weekly","Monthly","AtStartup","AtLogon" -prompt "Please Select Frequency of your Task" -listboxtype One) -Type (MultipleSelectionBox -inputarray "CMD","PowerShell","Program" -prompt "Please Select Type of Task" -listboxtype One)
-
---
+++ Create_Scheduled_Task.bat
@@ -0,0 +1,1 @@
+powershell.exe /executionpolicy bypass /file %~dp0\source\Create_Scheduled_Task.bat
